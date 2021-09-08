67287cac6dff84e68bbfc1eb475cb0cd534f1dff
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-09-08 15:09:54 +0200

    import pretty hierarchy asset

---
+++ Assets/Plugins/PrettyHierarchy-0.1.1/Utils/EditorUtils.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace PrettyHierarchy
+{
+    public static class EditorUtils
+    {
+        public static bool IsHierarchyFocused { get { return EditorWindow.focusedWindow != null && EditorWindow.focusedWindow.titleContent.text == "Hierarchy"; } }
+    }
+}
+#endif
