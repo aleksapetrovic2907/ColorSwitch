0cc505792aa359d37b33606e06786f38a74119fc
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-09-08 13:32:48 +0200

    rot tweens can have random direction

--- Assets/_Scripts/Tweens/Rotate.cs
+++ Assets/_Scripts/Tweens/Rotate.cs
@@ -9,13 +9,24 @@ namespace ColorSwitch.Tweens
         [Header("Rotation Settings")]
         [SerializeField] private float _rotationStart;
         [SerializeField] private float _rotationEnd;
+        [SerializeField] private bool _randomizeDirection = false;
 
         private Rigidbody2D _rigidbody2D;
 
         protected override void Awake()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            RandomizeDirection();
             base.Awake();
+        }
+
+        private void RandomizeDirection()
+        {
+            if (_randomizeDirection)
+            {
+                var range = new System.Random();
+                _rotationEnd *= range.Next(0, 2) > 0 ? 1 : -1;
+            }
         }
 
         public override void SetTween()
